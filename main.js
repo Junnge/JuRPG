66db8e1482d7f420ab64c28f282f75134ea41720
Alex <parttzippik@gmail.com>
2018-08-19 19:26:00 +0300

    code formating

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-function randomInt(min, max) {
+//ф-я для генерации цельных чисел в диапазоне [min, max]
+function randomInt(min, max) { 
 	var rand = min - 0.5 + Math.random() * (max - min + 1)
 	rand = Math.round(rand);
 	return rand;
 }
-var xp=0;
-var forage_cd=0;
 
-var arrEnemies = []; //Массив монстров
+//Массив монстров
+var arrEnemies = []; 
 arrEnemies[0] = {
 	name : "Койот",
 	hp : 25,
@@ -26,7 +26,8 @@ arrEnemies[2] = {
 	exp : 4
 }
 
-var enemyObject = new Object();
+//Пустой объект для заполнение его активным монстром 
+var enemyObject = new Object(); 
 
 enemyObject["name"] = "Врагов нет";
 enemyObject["hp"] = 0;
@@ -34,21 +35,23 @@ enemyObject["dmg"] = 0;
 enemyObject["exp"] = 0;
 
 
-function Player(name) {
+function Player(name){ //Описание класса игрока
 	this.name = name;
 	this.lvl = 0;
 	this.exp = 0;
 	this.skill_points = 0;
 	this.base_damage = 1;
 	
-	this.give_exp = function(x) {
+	//Ф-я начисления опыта и повышения уровня если достигнута нужная отметка
+	this.give_exp = function(x){ 
 		this.exp = this.exp + x;
 		while (this.exp >= this.get_next_lvl_exp()) {
 			this.lvlup();
 		}
 	}
 	
-	this.lvlup = function() {
+	//Ф-я повышения уровня героя
+	this.lvlup = function(){  
 		this.base_damage++;
 		this.skill_points++;
 		this.exp = this.exp - this.get_next_lvl_exp();
@@ -56,43 +59,51 @@ function Player(name) {
 		status_update("<p>Теперь вы "+this.lvl+" уровня</p>");
 	}
 	
-	this.get_next_lvl_exp = function() {
+	//Подсчет необходимиого кол-ва опыта для поднятия уровня 
+	this.get_next_lvl_exp = function(){ 
 		return 10 * (this.lvl + 1);
 	}
 }
 
+//Создание игрока
+var player = new Player('whoever'); 
 
-var player = new Player('whoever');
-
-function hunt() {
-	if (forage_cd==0) {
+//флаг кулдауна на действие 
+var hunt_cd=0; 
+//Охота на зверей
+function hunt() { 
+	if (hunt_cd==0){
 		player.give_exp(1);
 		status_update("<p>Вы поймали "+randomInt(2, 4)+" ящерицы и получили 1XP</p>");
-		forage_cd = 1;
-		setTimeout(function(){forage_cd=0;}, 10000);
+		hunt_cd = 1;
+		//Активация кулдауна способности 
+		setTimeout(function(){hunt_cd=0;}, 10000); 
 	} else status_update("<p>Вы устали!</p>");
 }
 
-function adventure(){
+//Пуьешествие в постоши, генерация событий 
+function adventure(){	
 	var id = randomInt(0, arrEnemies.length-1);
-	change_enemy(id)
+	change_enemy(id);
 	status_update("</p>Вы встретили ["+enemyObject.name+"]</p>");
 }
 
-function fight(){
+//Бой с монстром
+function fight(){ 
 	if(enemyObject.hp > 0){
 		var damage = player.base_damage + randomInt(1, 6);
 		enemyObject.hp -= damage;
 		if (enemyObject.hp<=0) {
 			enemyObject.hp=0;
-			kill_current_enemy()
+			kill_current_enemy();
 		} else {
 			status_update("<p>Вы нанесли ["+enemyObject.name+"] "+damage+" урона</p>");
 		}
 	}
 }
 
-function change_enemy(id){
+//Заполнения объекта монстра 
+function change_enemy(id){ 
 	if (id == -1) {
 		enemyObject["name"] = "Врагов нет";
 		enemyObject["hp"] = 0;
@@ -106,15 +117,16 @@ function change_enemy(id){
 	}
 }
 
-function kill_current_enemy(){
+//Смерть монстра, выдача опыта и сброс объкта
+function kill_current_enemy(){ 
 	player.give_exp(enemyObject.exp);
 	status_update("<p>Вы убили ["+enemyObject.name+"] и получили "+enemyObject.exp+" опыта</p>");
 	change_enemy(-1);      
 	status_update();  
 }
 
-
-function status_update(text) {
+//Вывод text в лог сообщений, обновление всех показателей на панелях
+function status_update(text) { 
 	if (text!=undefined) {document.getElementById('text_box').innerHTML += text;}	
 	document.getElementById('exp_bar').innerHTML = "Опыт: " + player.exp + " | " + player.get_next_lvl_exp();
 	document.getElementById('health_bar_enemy').innerHTML = "Здоровье: "+enemyObject.hp;
@@ -122,6 +134,7 @@ function status_update(text) {
 	document.getElementById('text_box').scrollTop = 9999;
 }
 
+//Отображение элементов интерфейса 
 function show_box(box, button) {
 document.getElementById('text_button').src = "img/buttons/button_unactive.png";
 document.getElementById('map_button').src = "img/buttons/button_unactive.png";
@@ -136,30 +149,27 @@ document.getElementById(button).src = "img/buttons/button_active.png";
 	if (box == 'text_box') {document.getElementById('action_buttons').style = "visibility: visible;"}
 }
 
-//очень страшная ф-я для обработки колесика мышки
+//Очень страшная ф-я для обработки колесика мышки
 var elem = document.getElementById('text_box');
 if (elem.addEventListener) {
-  if ('onwheel' in document) {
-    // IE9+, FF17+, Ch31+
-    elem.addEventListener("wheel", onWheel);
-  } else if ('onmousewheel' in document) {
-    // устаревший вариант события
-    elem.addEventListener("mousewheel", onWheel);
-  } else {
-    // Firefox < 17
-    elem.addEventListener("MozMousePixelScroll", onWheel);
-  }
-} else { // IE8-
-  elem.attachEvent("onmousewheel", onWheel);
-}
+	if ('onwheel' in document) {
+		// IE9+, FF17+, Ch31+
+		elem.addEventListener("wheel", onWheel);
+	} else if ('onmousewheel' in document) {
+		// устаревший вариант события
+		elem.addEventListener("mousewheel", onWheel);
+	} else {
+		// Firefox < 17
+		elem.addEventListener("MozMousePixelScroll", onWheel);
+	}
+	} else { // IE8-
+		elem.attachEvent("onmousewheel", onWheel);
+	}
 
 function onWheel(e) {
-  e = e || window.event;
-
-  var delta = e.deltaY || e.detail || e.wheelDelta;
-
-  var info = document.getElementById('text_box');
-
-  document.getElementById('text_box').scrollTop += delta*3;
-  e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+	e = e || window.event;
+	var delta = e.deltaY || e.detail || e.wheelDelta;
+	var info = document.getElementById('text_box');
+	document.getElementById('text_box').scrollTop += delta*3;
+	e.preventDefault ? e.preventDefault() : (e.returnValue = false);
 }
