da95b15851a15229a8e91d14a0b1546978ab5f5a
Alex <parttzippik@gmail.com>
2018-08-19 15:02:18 +0300

    scrollbar removed

--- main.js
+++ main.js
@@ -135,3 +135,31 @@ document.getElementById(button).src = "button_active.png";
 
 	if (box == 'text_box') {document.getElementById('action_buttons').style = "visibility: visible;"}
 }
+
+//очень страшная ф-я для обработки колесика мышки
+var elem = document.getElementById('text_box');
+if (elem.addEventListener) {
+  if ('onwheel' in document) {
+    // IE9+, FF17+, Ch31+
+    elem.addEventListener("wheel", onWheel);
+  } else if ('onmousewheel' in document) {
+    // устаревший вариант события
+    elem.addEventListener("mousewheel", onWheel);
+  } else {
+    // Firefox < 17
+    elem.addEventListener("MozMousePixelScroll", onWheel);
+  }
+} else { // IE8-
+  elem.attachEvent("onmousewheel", onWheel);
+}
+
+function onWheel(e) {
+  e = e || window.event;
+
+  var delta = e.deltaY || e.detail || e.wheelDelta;
+
+  var info = document.getElementById('text_box');
+
+  document.getElementById('text_box').scrollTop += delta*3;
+  e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+}
