4be3dc91ed5362ce64fd0f94d55fd16dfbe5d129
Alex <parttzippik@gmail.com>
2018-08-21 03:57:53 +0300

    New UI tabs in STATS

--- main.js
+++ main.js
@@ -130,9 +130,9 @@ function Inv() {
 	}
 
 	this.show = function(){
-		document.getElementById('stats_box').innerHTML = '';
+		document.getElementById('inv_box').innerHTML = '';
 		for (var item in this.stuff) {
-			document.getElementById('stats_box').innerHTML += '<p>'+arrItems[item].name+" ("+this.stuff[item]+")</p>";
+			document.getElementById('inv_box').innerHTML += '<p>'+arrItems[item].name+" ("+this.stuff[item]+")</p>";
 		}
 	}
 }
@@ -199,20 +199,30 @@ function status_update(text) {
 
 
 //Отображение элементов интерфейса 
-function show_box(box, button) {
+function show_box(box, button, button2) {
 document.getElementById('action_button').src = "img/buttons/button_unactive.png";
 document.getElementById('map_button').src = "img/buttons/button_unactive.png";
 document.getElementById('stats_button').src = "img/buttons/button_unactive.png";
 document.getElementById('action_box').style = "visibility: hidden;";
 document.getElementById('map_box').style = "visibility: hidden;";
-document.getElementById('stats_box').style = "visibility: hidden;";
+//document.getElementById('stats_box').style = "visibility: hidden;";
 document.getElementById('action_buttons').style = "visibility: hidden;";
 document.getElementById('stats_buttons').style = "visibility: hidden;";
+document.getElementById('hero_box').style = "visibility: hidden;";
+document.getElementById('inv_box').style = "visibility: hidden;";
+document.getElementById('statistics_box').style = "visibility: hidden;";
+//document.getElementById('settings_box').style = "visibility: hidden;";
 document.getElementById(box).style = "visibility: visible;"
 document.getElementById(button).src = "img/buttons/button_active.png";
 
 	if (box == 'action_box') {document.getElementById('action_buttons').style = "visibility: visible;"}
-	if (box == 'stats_box') {document.getElementById('stats_buttons').style = "visibility: visible;"}
+	if (box == 'hero_box' || box == 'inv_box' || box == 'statistics_box' ) {
+		document.getElementById('stats_buttons').style = "visibility: visible;"
+		if (button2 == 'hero_button') {document.getElementById('hero_box').style = "visibility: visible;"}
+		if (button2 == 'inv_button') {document.getElementById('inv_box').style = "visibility: visible;"}
+		if (button2 == 'statistics_button') {document.getElementById('statistics_box').style = "visibility: visible;"}
+		//if (button2 == 'settings_button') {document.getElementById('settings_box').style = "visibility: visible;"}
+	}
 }
 
 //Очень страшная ф-я для обработки колесика мышки
