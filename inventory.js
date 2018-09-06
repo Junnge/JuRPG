41c8f7b452f47af91e5df5705b8f79861d2cb621
lordnord <dlx1989@gmail.com>
2018-09-06 16:33:42 +0300

    inventory as as separate file

---
+++ inventory.js
@@ -0,0 +1,53 @@
+// dependencies: Item
+
+class Inv {
+	constructor(){
+		this.stuff = {};
+	}
+	
+	remove(item, count){
+		if (this.stuff[item] > count){
+			this.stuff[item] -= count;
+		} else if (this.stuff[item] == count){
+			delete this.stuff[item];
+		} else {
+			throw "Not enough items";
+		}
+	}
+	
+	add(item, count){
+		if (item in this.stuff) {
+			this.stuff[item] += count
+		} else {
+			this.stuff[item] = count
+		}
+	}
+	
+	buy(item, price, amount){
+		// order of following 2 lines is important
+		this.remove("cap", price*amount);
+		this.add(item, amount);
+	}
+
+	sell(item, price, amount){
+		// order of these 2 lines is also important
+		this.remove(item, amount);
+		this.add("cap", price*amount);
+	}
+	
+	open_container(container_item_id){
+		this.remove(container_item_id, 1);
+		var items = new Item(container_item_id).container;
+		for (var id in items){
+			var amount = items[id];
+			this.add(id, amount)
+		}
+			
+	save(){
+		localStorage.inv = JSON.stringify(this.stuff);
+	}
+	
+	load(){
+		this.stuff = JSON.parse(localStorage.inv);
+	}
+}
